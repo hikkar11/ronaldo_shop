c255f0e94d7ecf0d64acb47d64b25c370c5135c3
Aminev Kirill <a.shizzo432@gmail.com>
2025-06-05 00:51:17 +0500

    first commit

---
+++ src/main/java/com/example/ronaldoshop/RonaldoShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.ronaldoshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RonaldoShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RonaldoShopApplication.class, args);
+    }
+
+}
